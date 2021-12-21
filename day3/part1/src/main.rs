39b710028e04d58a0952f74f661444613403352f
Alastair Campbell <alastair.campbell@bdslive.com>
2021-12-21 11:40:08 +0000

    More work on day 3 part 1.

--- day3/part1/src/main.rs
+++ day3/part1/src/main.rs
@@ -4,7 +4,28 @@ fn main() {
     let input = fs::read_to_string("input.txt")
         .expect("Could not read input file");
 
-    for line in input.lines() {
-        println!("{}", line.chars().nth(0).expect("Invalid string"));
+    let len = input.lines().next().unwrap().chars().count();
+    let mut gamma:u32 = 0;
+
+    for n in 0..len {
+        let mut zero_count = 0;
+        let mut one_count = 0;
+
+        for line in input.lines() {
+            match line.chars().nth(n).unwrap() {
+                '0' => zero_count += 1,
+                '1' => one_count += 1,
+                _ => panic!("Invalid character"),
+            }
+        }
+
+        gamma = gamma << 1;
+
+        if one_count > zero_count {
+            gamma += 1;
+        }
     }
+
+    let epsilon = !gamma;
+    println!("Gamma: {} Epsilon: {}", gamma, epsilon);
 }
